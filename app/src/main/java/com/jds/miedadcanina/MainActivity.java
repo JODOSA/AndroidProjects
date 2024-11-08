246d11e8eea837afa77585e0f904bf02ecc16e45
Joaquín <32976960+JODOSA@users.noreply.github.com>
2024-11-09 00:35:51 +0100

    Commit num 2

--- app/src/main/java/com/jds/miedadcanina/MainActivity.java
+++ app/src/main/java/com/jds/miedadcanina/MainActivity.java
@@ -1,6 +1,10 @@
 package com.jds.miedadcanina;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText ageEdit = findViewById((R.id.age_edit));
+        TextView resultText = findViewById((R.id.result_text));
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> {
+            String age = ageEdit.getText().toString();
+            int ageInt = Integer.parseInt(age);
+
+            int result = ageInt * 7;
+            String resultString = "Si fueras perro, tu edad sería " + result + " años";
+            resultText.setText(resultString);
         });
     }
 }
